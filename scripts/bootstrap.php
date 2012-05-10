5d239d6d0949a6b97f15074e827b6a9ae8b0a9b5
Vladimir <TheRatW@gmail.com>
2012-05-10 01:38:31 -0700

    Miaox - fix bootstrap

--- scripts/bootstrap.php
+++ scripts/bootstrap.php
@@ -1,7 +1,14 @@
 <?php
-$config = include dirname( __FILE__ ) . '/../data/config.php' ;
-require_once PROJECT_MIAO_ROOT . '/modules/Autoload/classes/Autoload.class.php';
+$config = include dirname( __FILE__ ) . '/../data/config.php';
+foreach ( $config[ 'libs' ] as $value )
+{
+	if ( 'Miao' == $value[ 'name' ] )
+	{
+		require_once $value[ 'path' ] . '/modules/Autoload/classes/Autoload.class.php';
+		break;
+	}
+}
 
-Miao_Autoload::register( $config['libs'] );
+Miao_Autoload::register( $config[ 'libs' ] );
 Miao_Path::register( $config );
 Miao_Env::defaultRegister();
